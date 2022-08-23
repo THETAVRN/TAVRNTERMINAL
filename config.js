37b1d263185ce78c02f3bb0b55b2fbffe87a8a28
THETAVRN <111832725+THETAVRN@users.noreply.github.com>
2022-08-23 00:23:12 -0400

    Update and rename example-config.js to config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+	BOT_TOKEN: 'MTAxMTQ3MjMwNDM3NjY0NzcyMQ.Gqj6Eh.2sn-6GPJ6VWAOOk8aS3kciootRrzBbc4C01_S8',
+	MORNING_BELL_CHANNEL_ID: '740746901355495484D'
+};
+
+module.exports = config;
